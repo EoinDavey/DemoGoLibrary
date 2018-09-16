9cfc0fdbeb20bf97ed9abeb1d170769dd2721ffc
Maciej Goszczycki <mgoszcz2@gmail.com>
2018-09-16 05:14:35 -0400

    More clients

--- lib.go
+++ lib.go
@@ -9,3 +9,18 @@ type HttpClient struct {
 
 func NewClient(address string) HttpClient {
 }
+
+func OldClient(address string) HttpClient {
+}
+
+func AmazingClient(address string) HttpClient {
+}
+
+func SpeactularClient(address string) HttpClient {
+}
+
+func ElderlyClient(address string) HttpClient {
+}
+
+func TcpClient(address string) HttpClient {
+}
