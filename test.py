ebad1a3e820342ae73703e0d8651eccf80bdc5d0
Maciej Goszczycki <mgoszcz2@gmail.com>
2018-09-16 05:15:01 -0400

    Yey

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
 def testFunc(testparam):
     pass
+def client(testparam):
+    pass
+
+def oldclient(testparam):
+    pass
+
+def foo(testparam):
+    pass
+def bar(testparam):
+    pass
+def verylongname(testparam):
+    pass
+def haha(testparam):
+    pass
